dc9d12727e3155bea179a957e00ab985592a3b37
Davvi Duarte Rodrigues <davvi.duarte@gmail.com>
2023-12-04 23:25:33 -0300

    Projeto de página de login - criação de componentes básicos, css básico dos campos e alinhamento responsivo

---
+++ my-login/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPages';
+
+
+function App() {
+  return (
+    <div className="app">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
